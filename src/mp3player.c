1ef2ade4214160d4744fa3539a4b2e6ad144db74
RonnieHarrod-cell <strykeryt001@gmail.com>
2026-08-09 12:46:56 +0100

    fix(mp3player): drop unused BOREDOS_APP_* metadata
    Signed-off-by: RonnieHarrod-cell <strykeryt001@gmail.com>

--- src/mp3player.c
+++ src/mp3player.c
@@ -2,9 +2,6 @@
 // This software is released under the GNU General Public License v3.0. See LICENSE file for details.
 // This header needs to maintain in any file it is present in, as per the GPL license terms.
 
-// BOREDOS_APP_DESC: Simple GUI MP3 player (minimp3 + /dev/dsp)
-// BOREDOS_APP_ICONS: /Library/images/icons/serenityicons/32x32/app-music.png
-
 #include "ntk.h"
 
 #include <stdio.h>
